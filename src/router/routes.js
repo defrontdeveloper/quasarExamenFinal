a46081768e192796e9036150fd42c145ed1ebb87
David <defrontdeveloper@gmail.com>
2023-07-15 19:49:38 -0500

    fin

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,20 +1,15 @@
 
 const routes = [
   { path: '', component: () => import('pages/Login.vue') },
-
-
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '/inicio', component: () => import('pages/Inicio.vue') },
+      { path: '/', component: () => import('pages/Inicio.vue') },
       { path: '/crear-usuarios', component: () => import('pages/Crear-Usuarios.vue') },
       { path: '/actividades', component: () => import('pages/Actividades.vue') },
     ]
   },
-
-
-
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
